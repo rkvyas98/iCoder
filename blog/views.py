73e812491b5976d064b0c450d79c9b7409d1a75d
Rakshit Vyas <rkvyas98@gmail.com>
2020-07-13 09:23:30 +0530

    mid commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Post, BlogComment
+from django.contrib import messages
+from blog.templatetags import extras
 
 # Create your views here.
+    
+def blogHome(request):
+    allposts = Post.objects.all()
+    context= {
+        'allposts' : allposts
+    }
+    return render(request, 'blog/blogHome.html', context)
+
+def blogPost(request, slug):
+    post = Post.objects.filter(slug = slug).first()
+    commments = BlogComment.objects.filter(post = post ,parent = None)
+    replies = BlogComment.objects.filter(post = post).exclude(parent = None)
+    replyDict = {}
+    for reply in replies:
+        if reply.parent.sno not in replyDict.keys():
+            replyDict[reply.parent.sno] = [reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
+        print(replyDict)
+    context ={
+        'post' : post, 
+        'comments' : commments,
+        'user' : request.user,
+        'replyDict' :replyDict,
+    } 
+    return render(request, 'blog/blogPost.html', context)
+
+def postComment(request):
+    if request.method == 'POST':
+        comment = request.POST.get("comment")
+        user = request.user
+        postSno = request.POST.get("postSno")
+        parentSno = request.POST.get("parentSno")
+        post = Post.objects.get(sno = postSno)
+        if parentSno == "":
+            comment = BlogComment(comment=comment, user=user, post=post)
+            comment.save()
+            messages.success(request, 'Your comment has been posted successfully!')
+        
+        else:
+            parent = BlogComment.objects.get(sno = parentSno)
+            comment = BlogComment(comment=comment, user=user, post=post, parent=parent )
+            comment.save()
+            messages.success(request, 'Your reply has been posted successfully!')
+
+    return redirect(f'/blog/{post.slug}')
---
+++ home/views.py
@@ -0,0 +1,111 @@
+from django.shortcuts import render, HttpResponse, redirect
+from home.models import Contact
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from blog.models import Post
+
+# HTML pages here.
+
+def home(request):
+    return render(request, 'home/home.html')
+    
+def contact(request):
+    if request.method=='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name, email,phone,content)
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<5:
+            messages.error(request, "Please fill all the fields of form!")
+
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been sent successfully!")
+
+    return render(request, 'home/contact.html')
+
+def about(request):
+    messages.error(request, 'This is about')
+    return render(request, 'home/about.html')
+
+def search(request):
+    query = request.GET['query']
+    if len(query) > 80:
+        allposts = Post.objects.none()
+    else:
+        allpostsTitle = Post.objects.filter(title__icontains=query)
+        allpostsContent = Post.objects.filter(content__icontains=query)
+        allposts = allpostsTitle.union(allpostsContent)
+
+    if allposts.count() == 0:
+        messages.error(request, 'No search reults found. Please refine your query!')
+
+    params ={
+        'allposts' : allposts,
+        'query' : query
+    }
+    return render(request, 'home/search.html', params)
+
+# Authentication APIs
+
+def handleSignup(request):
+    if request.method == 'POST':
+        # Getting post params
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # check for inputs
+        if len(username) > 10:
+            messages.error(request, "Username should contain less than 10 characters.")
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, "Username should only contain letters and numbers.")
+            return redirect('home')
+
+        if pass1 != pass2:
+            messages.error(request, "Passwords donot match. ")
+            return redirect('home')
+
+        # creating users
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your iCoder account has been successfully created!")
+
+        return redirect('home')
+
+    else:
+        return HttpResponse("404 - Not Found")
+
+def handleLogin(request):
+    if request.method == 'POST':
+        # Getting post params
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username = loginusername, password = loginpassword)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In!")
+            return redirect('home')
+        
+        else:
+            messages.error(request, "Invalid Credentials, Please Try Again!")
+            return redirect('home')
+
+    return HttpResponse('404 - Not Found')
+    
+def handleLogout(request):
+    logout(request)
+    messages.success(request, "Successfully Logged Out!")
+    return redirect('home')
